4eba681bc9754c6b44091146779b6eaaed8687d0
MdAfsarHossain <afsar291722@gmail.com>
2026-06-13 00:34:02 +0600

    feat: Link and programmatic navigation code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from '@/components/Navbar'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        <main className="min-h-dvh">
+        {children}
+        </main>
+        <footer className="text-center text-4xl my-5">
+          This is Footer Section
+        </footer>
+        </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
+'use client'
+// import Link from "next/link";
+
+import { useRouter } from "next/navigation";
+
 const HomePage = () => {
     console.log('Hello World From the Home Page');
-    
+   
+    const router = useRouter();
+
+    const handleNavigation = () => {
+        router.push('/dashboard')
+    }
+
     return (
         <div className="text-center flex flex-col justify-center items-center gap-4 min-h-screen">
             <h1 className="text-4xl">Welcome Next JS Home Page</h1>
             <p className="text-6xl">React Server Component (RSC)</p>
+            <div>
+                {/* OPTION : 1 */}
+                {/* <Link href='/dashboard'><button>Dashboard</button></Link> */}
+
+                {/* OPTION: 2 */}
+                <button onClick={handleNavigation}>Dashboard</button>
+            </div>
         </div>
     );
 };
