08e182479bebea4e01ebca62a261aee188fbc624
MdAfsarHossain <afsar291722@gmail.com>
2026-06-12 18:06:16 +0600

    Contact, Dashboard, Admin, Profile page added

---
+++ app/dashboard/profile/admin/page.tsx
@@ -0,0 +1,9 @@
+const AdminPage = () => {
+    return (
+        <div>
+            <h1 className='text-4xl'>Admin Page</h1>
+        </div>
+    );
+};
+
+export default AdminPage;
---
+++ app/dashboard/profile/page.tsx
@@ -0,0 +1,9 @@
+const ProfilePage = () => {
+    return (
+        <div>
+            <h1 className='text-4xl'>Profile Page</h1>
+        </div>
+    );
+};
+
+export default ProfilePage;
