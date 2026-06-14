17153b6afe65ac7de9cb39df0b1281dee68719fc
MdAfsarHossain <afsar291722@gmail.com>
2026-06-14 18:14:37 +0600

    feat: Handling Active links.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,13 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 
 const Navbar = () => {
+    const pathname = usePathname();
+    console.log(pathname);
+    
     return (
         <nav className="w-full bg-gray-900 text-white px-6 py-3 flex items-center justify-between">
             <div className="text-xl font-bold">
@@ -10,8 +16,8 @@ const Navbar = () => {
 
             <div className="flex justify-center items-center space-x-6">
                 <input className="bg-white rounded-2xl py-1 text-black px-2" placeholder="Search..."></input>
-                <Link href="/products" className="hover:text-gray-300">Products</Link>
-                <Link href="/about" className="hover:text-gray-300">About</Link>
+                <Link href="/products" className={pathname === '/products' ? "underline text-blue-800" : "hover:underline text-gray-100"}>Products</Link>
+                <Link href="/about" className={pathname === '/about' ? "underline text-blue-800" : "hover:text-gray-300"}>About</Link>
                 <Link href="/contact" className="hover:text-gray-300">Contact</Link>
                 <Link href="/dashboard" className="hover:text-gray-300">Dashboard</Link>
                 <Link href="/gallery" className="hover:text-gray-300">Gallery</Link>
